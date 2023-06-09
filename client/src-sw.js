072cdabf83b0838080f18dad7ca4b696e1ccaed8
Abdirahman Shukri <abdirahmanshukri@Abdirahmans-MacBook-Air.local>
2023-06-09 14:42:13 -0500

    finished files

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,6 +1,37 @@
-const { warmStrategyCache } = require('workbox-recipes');
+const { warmStrategyCache } = require("workbox-recipes");
 const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
-const { registerroute} = require("workbox-routing");
+const { registerroute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
 const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: "pages",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
+});
+warmStrategyCache({
+  urls: ["/index.html", "/"],
+  strategy: pageCache,
+});
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
+
+//TODO: implement assest caching
+registerRoute(
+  new registerRoute({
+    cacheName: "assestCache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
